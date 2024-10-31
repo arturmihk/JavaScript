974282e993e4d938b77f1b79306ebd0f67d1efcf
arturmihk <115464760+arturmihk@users.noreply.github.com>
2024-10-31 13:20:24 +0200

    Add files via upload

---
+++ JavaScript/01/yl01.js
@@ -0,0 +1,24 @@
+/*
+Artur-Mihk Peterson
+31.10.2024
+yl01
+*/
+
+
+const x = 4.2;
+const y = "'Will Code 4 food'";
+const z = '"123"';
+const w = true;
+const arr = [1,2,3];
+const f = "FALSE";
+const t = "'true'";
+const und = undefined;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(w);
+console.log(arr);
+console.log(f);
+console.log(t);
+console.log(und);
---
+++ JavaScript/02/yl02.js
@@ -0,0 +1,18 @@
+/*
+Artur-Mihk Peterson
+31.10.2024
+yl02
+*/
+//Kellaaeg
+let tunnid = 2;
+let minutid = 12;
+let sekundid = 49;
+let kell = tunnid + ":" + minutid + ":" + sekundid;
+console.log(kell);
+//Tsitaat
+let tsitaat = "Alati on raskem võidelda usu, kui teadmiste vastu. -Adolf Hitler";
+console.log(tsitaat);
+//Mall
+let enimi = "Tõnis";
+let pnimi = "Kändmaa";
+console.log(enimi,pnimi,"nimetähed on:",enimi.charAt(0),pnimi.charAt(0));
